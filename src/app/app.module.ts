62eb8df7b4978f1d5d6998ee7fd989abab2aaa71
davidrodriguezonetec <david.rodriguez@grupoonetec.com>
2019-01-23 09:34:49 +0100

    bajando imágenes de firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,25 +6,45 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { SubirPage } from '../pages/subir/subir';
+
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule, AngularFireDatabase } from '@angular/fire/database';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyDVgd3IqG880Vd6bmfovdEJHXqKgf_HT5c",
+  authDomain: "gag-5d3f0.firebaseapp.com",
+  databaseURL: "https://gag-5d3f0.firebaseio.com",
+  projectId: "gag-5d3f0",
+  storageBucket: "gag-5d3f0.appspot.com",
+  messagingSenderId: "979955400889"
+};
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    SubirPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    SubirPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    AngularFireDatabase,
+    { provide: ErrorHandler, useClass: IonicErrorHandler }
   ]
 })
-export class AppModule {}
+export class AppModule { }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,25 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { ModalController } from 'ionic-angular';
+import { SubirPage } from '../subir/subir';
+import { AngularFireDatabase } from '@angular/fire/database';
+import { Observable } from 'rxjs-compat';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  posts: Observable<any[]>;
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+    private modalCtrl: ModalController,
+    private afDB: AngularFireDatabase
+  ) {
+    this.posts = afDB.list('post').valueChanges();
+  }
 
+  mostrarModal(){
+    let modal = this.modalCtrl.create(SubirPage).present();
   }
 
 }
